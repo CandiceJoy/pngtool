b00f07adec85faf70285baefe3d0d9d506e0684d
CandiceJoy <candice@candicejoy.com>
2023-01-03 16:41:59 -0500

    Initial commit

---
+++ pngtool/Program.cs
@@ -0,0 +1,28 @@
+﻿using ImageMagick;
+
+class Program
+{
+	static void Main( string[] args )
+	{
+		string file      = args[0];
+		int   colorType = int.Parse( args[1] );
+
+		if( !File.Exists( file ) )
+		{
+			Console.WriteLine( "Cannot find file " + file );
+			return;
+		}
+
+		Console.WriteLine("Desired color type: " + colorType + "("+(ImageMagick.ColorType)colorType+")");
+
+		MagickImage image = new MagickImage( file );
+
+		Console.WriteLine("Size: "           + image.Width          + "x" + image.Height);
+		Console.WriteLine("Old color type: " + (int)image.ColorType + "(" +image.ColorType +")");
+
+		image.ColorType = (ImageMagick.ColorType)colorType;
+		image.Write( file );
+		image = new MagickImage( file );
+		Console.WriteLine("New color type: " + (int)image.ColorType + "("+image.ColorType+")");
+	}
+}
